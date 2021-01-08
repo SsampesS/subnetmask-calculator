4e6d44d1fc863fa4385993b289bd6d5f7a55e48e
Sami Salminen <59646985+SsampesS@users.noreply.github.com>
2021-01-08 12:47:56 +0200

    Update decimalConverter.cpp

--- decimalConverter.cpp
+++ decimalConverter.cpp
@@ -14,7 +14,7 @@ string decimalConverter(int decOctet)
 	case 1:
 	{
 		binaryOctet.append("00000001");											// doing the conversion of "1" here because 
-		return binaryOctet;														// it caused issues in the main conversion.
+		return binaryOctet;												// it caused issues in the main conversion.
 	}
 	default:
 	{
@@ -23,18 +23,18 @@ string decimalConverter(int decOctet)
 			if (division != 1 && division == decOctet)
 			{
 				binaryOctet += to_string(division % 2);							// executing the first division
-				division = decOctet / 2;										// binary added to binaryOctet
+				division = decOctet / 2;								// binary added to binaryOctet
 				binaryOctet += to_string(division % 2);
 			}
 			if (division == 1)
 			{
 				while (octLen != binaryOctet.length())							// adding the excempted zeroes to the binary. 
 					binaryOctet.append("0");
-				reverse(binaryOctet.begin(), binaryOctet.end());				// reversing the string to show the binary in a 'correct' way
+				reverse(binaryOctet.begin(), binaryOctet.end());					// reversing the string to show the binary in a 'correct' way
 				break;
 			}
 			else
-				division = division / 2;										// executing subsequent divisions
+				division = division / 2;								// executing subsequent divisions
 
 			binaryOctet += to_string(division % 2);								// binary added to binaryOctet
 		}
